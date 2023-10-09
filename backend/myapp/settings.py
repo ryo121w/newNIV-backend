35f3f6b20323c47eb6435f9d75f0acde09be8c86
ryo121w <ryoutarou9121@icloud.com>
2023-10-09 09:17:19 +0900

    commit

--- backend/myapp/settings.py
+++ backend/myapp/settings.py
@@ -58,7 +58,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://ryo121w.github.io"
+]
+
 
 ROOT_URLCONF = "myapp.urls"
 
